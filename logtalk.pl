96374c6df9d3a9513c3080a967cce1e063da5c5e
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2013-12-12 19:51:08 +0100

    Updated logtalk location

--- logtalk.pl
+++ logtalk.pl
@@ -1,6 +1,6 @@
 %:- setenv('LOGTALKUSER', '/home/jan/logtalk').
-:- setenv('LOGTALKUSER', '/home/jan/src/lgtsvn').
-:- setenv('LOGTALKHOME', '/home/jan/src/lgtsvn').
+:- setenv('LOGTALKUSER', '/home/janw/3rdparty/logtalk3').
+:- setenv('LOGTALKHOME', '/home/janw/3rdparty/logtalk3').
 
 :- load_files('$LOGTALKHOME/integration/logtalk_swi',
 	      [ silent(true),
