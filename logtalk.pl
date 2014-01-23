99f84c1afae309ad1ff9b5fd7754f00b9df01fd3
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2014-01-23 19:03:45 +0100

    Dynamic logtalk definition

--- logtalk.pl
+++ logtalk.pl
@@ -1,6 +1,12 @@
-%:- setenv('LOGTALKUSER', '/home/jan/logtalk').
-:- setenv('LOGTALKUSER', '/home/janw/3rdparty/logtalk3').
-:- setenv('LOGTALKHOME', '/home/janw/3rdparty/logtalk3').
+set_logtalk_environment :-
+	getenv('LOGTALKHOME', Dir),
+	exists_directory(Dir), !.
+set_logtalk_environment :-
+	expand_file_name('~/3rdparty/logtalk3', [Dir]),
+	setenv('LOGTALKUSER', Dir),
+	setenv('LOGTALKHOME', Dir).
+
+:- set_logtalk_environment.
 
 :- load_files('$LOGTALKHOME/integration/logtalk_swi',
 	      [ silent(true),
