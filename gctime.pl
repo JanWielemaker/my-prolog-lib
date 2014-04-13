642f3bff84a9a159553289c715c4275765e2803c
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2014-04-13 22:39:37 +0200

    Extended gctime/1,3

--- gctime.pl
+++ gctime.pl
@@ -25,19 +25,24 @@ gctime(Goal) :-
 
 gctime(Name, N, Goal0) :-
 	expand_goal(Goal0, Goal),
-	statistics(garbage_collection, [GCN0, GCBytes0, GCTime0, GCLeft0]), 
-	statistics(cputime, OldTime), 
+	statistics(atom_garbage_collection, [AGCN0, AGCAtoms0, AGCTime0]),
+	statistics(garbage_collection, [GCN0, GCBytes0, GCTime0, GCLeft0]),
+	get_time(Wall0),
+	statistics(cputime, OldTime),
 	(   N =:= 1
 	->  (   catch(Goal, E, true)
 	    ->  Result = yes
 	    ;   Result = no
 	    )
 	;   forall(between(1, N, _), Goal),
-	    Result = yes 
+	    Result = yes
 	),
-	statistics(cputime, NewTime), 
-	statistics(garbage_collection, [GCN1, GCBytes1, GCTime1, GCLeft1]), 
-	UsedTime is (NewTime - OldTime)/N, 
+	statistics(cputime, NewTime),
+	get_time(Wall1),
+	statistics(garbage_collection, [GCN1, GCBytes1, GCTime1, GCLeft1]),
+	statistics(atom_garbage_collection, [AGCN1, AGCAtoms1, AGCTime1]),
+	Wall is (Wall1-Wall0)/N,
+	UsedTime is (NewTime - OldTime)/N,
 	GCN is round((GCN1 - GCN0)/N),
 	_GCBytes is GCBytes1 - GCBytes0,
 	GCTime is (GCTime1 - GCTime0)/(1000*N),
@@ -45,25 +50,41 @@ gctime(Name, N, Goal0) :-
 	->  GCAvgLeft is round((GCLeft1 - GCLeft0)/(GCN1 - GCN0))
 	;   GCAvgLeft = 0
 	),
-	format('% Time, #GC, GC Left, GCTime, AvgC, AvgCl, AvgI~n'),
-	log('~w & ~2f & ~D & ~D & ~2f ',
-	       [Name, UsedTime, GCN, GCAvgLeft, GCTime]),
+	AGCN is round((AGCN1-AGCN0)/N),
+	AGCTime is (AGCTime1-AGCTime0)/(1000*N),
+	AGCAtoms is round((AGCAtoms1-AGCAtoms0)/N),
+	title,
+	log('~w & ~3f & ~3f & ~D & ~D & ~3f & ~D & ~D & ~3f',
+	    [ Name, UsedTime, Wall,
+	      GCN, GCAvgLeft, GCTime,
+	      AGCN, AGCAtoms, AGCTime
+	    ]),
 	gc_statistics_(N),
 	(   nonvar(E)
 	->  throw(E)
 	;   Result == yes
 	).
 
+:- if(current_predicate(gc_statistics/1)).
+title :-
+	format('% Test, CPU, Wall, #GC, GC Left, GCTime, \c
+	          #AGC, #Atoms, AGCTime, AvgC, AvgCl, AvgI~n').
+
 gc_statistics_(_N) :-
-	catch(gc_statistics(S), _, fail), !,
-%	writeln(S),
+	gc_statistics(S),
 	S = gc(Envs, Conts, AltClauses, Instr),
 	AvgC is Conts/Envs,
 	AvgCl is AltClauses/Envs,
 	AvgI is Instr/Conts,
 	log('& ~2f & ~2f & ~2f \\\\~n',
 	    [AvgC, AvgCl, AvgI]).
-gc_statistics_(_).
+:- else.
+title :-
+	format('% Test, CPU, Wall, #GC, GC Left, GCTime, #AGC, #Atoms, AGCTime~n').
+
+gc_statistics_(_) :-
+	log('\\\\~n', []).
+:- endif.
 
 log(Fmt, Args) :-
 	format(Fmt, Args),
