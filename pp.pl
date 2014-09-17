b51981e8c4104cb228481f9e7658c1ef733a9c3f
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2014-09-17 19:53:56 +0200

    Use system pretty-print library

--- pp.pl
+++ pp.pl
@@ -1,192 +1,56 @@
-/*  $Id$
+/*  Part of SWI-Prolog
 
-    Part of SWI-Prolog SGML/XML parser
+    Author:        Jan Wielemaker
+    E-mail:        J.Wielemaker@cs.vu.nl
+    WWW:           http://www.swi-prolog.org
+    Copyright (C): 2014, VU University Amsterdam
 
-    Author:  Jan Wielemaker
-    E-mail:  jan@swi.psy.uva.nl
-    WWW:     http://www.swi.psy.uva.nl/projects/SWI-Prolog/
-    Copying: LGPL-2.  See the file COPYING or http://www.gnu.org
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
 
-    Copyright (C) 1990-2000 SWI, University of Amsterdam. All rights reserved.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+    As a special exception, if you link this library with other files,
+    compiled with a Free Software compiler, to produce an executable, this
+    library does not by itself cause the resulting executable to be covered
+    by the GNU General Public License. This exception does not however
+    invalidate any other reasons why the executable file might be covered by
+    the GNU General Public License.
 */
 
 :- module(pp,
 	  [ pp/1,			% +Term
-	    pretty_print/1,		% +Term
-	    pretty_print/2		% +Stream, +Term
+	    dump_var/2			% +Name, +Term
 	  ]).
-:- require([ atom_length/2
-	   , between/3
-	   , forall/2
-	   , is_list/1
-	   , member/2
-	   , memberchk/2
-	   , portray_clause/1
-	   ]).
+:- use_module(library(pprint)).
+:- use_module(library(ansi_term)).
+
+user:goal_expansion(pp(Term), dump_var(Name, Term)) :-
+	var_property(Term, name(Name)).
 
 pp(Term) :-
-	pretty_print(Term).
+	print_term(Term, []).
 
-pretty_print(Stream, Term) :-
-	current_output(Old),
-	set_output(Stream),
-	pretty_print(Term),
-	set_output(Old).
+dump_var(Name, Value) :-
+	with_output_to(user_error, dump_var2(Name, Value)).
 
-pretty_print(Term) :-
-	nonvar(Term),
-	Term = (_Head :- _Body), !,
-	portray_clause(Term).
-pretty_print(Term) :-
-	numbervars(Term, 0, _, [attvar(skip)]),
-	pp(Term, 0),
-	write('.'), nl, fail.
-pretty_print(_).
-
-
-pp(Term, _Indent) :-
-	atomic(Term), !,
-	format('~W', [Term, [portray(true),quoted(true)]]).
-pp(Var, _Indent) :-
-	var(Var), !,
-	write(Var).
-pp(Var, _Indent) :-
-	Var = '$VAR'(_), !,
-	print(Var).
-pp('$aref'(Name), _Indent) :- !,
-	write(Name).
-pp(Module:Term, Indent) :-
-	atomic(Module), !,
-	writeq(Module), write(:),
-	pp(Term, Indent).
-pp([A1 := V1|ArgList], Indent) :-	% [] is done by `atomic'!
-	is_list(ArgList),
-	forall(member(A, ArgList), A = (_ := _)), !,
-	longest_attribute([A1 := V1|ArgList], 0, L),
-	NewIndent is Indent + 2,
-	(   L > 9, Indent < 25, length(ArgList, Args), Args > 1
-	->  ArgIndent is Indent + 4,
-	    ValGoal = (nl, indent(ArgIndent))
-	;   ArgIndent is Indent + 6 + L,
-	    ValGoal = write(' ')
-	),
-	write('[ '),
-	pp(A1, Indent), term_length(A1, L1),
-	tab(L-L1), write(' :='), ValGoal,
-	pp(V1, ArgIndent),
-	forall(member(A := V, ArgList),
-	       (write(','), nl,
-		indent(NewIndent),
-		pp(A, Indent), term_length(A, LA), tab(L-LA),
-		write(' :='), ValGoal, pp(V, ArgIndent))),
-	nl,
-	indent(Indent),
-	write(']').
-pp([H|T], Indent) :-
-	is_list(T), !,
-	write('[ '),
-	NewIndent is Indent + 2,
-	pp(H, NewIndent),
-	forall(member(E, T),
-	       (write(','), nl,
-		indent(NewIndent),
-		pp(E, NewIndent))),
-	nl,
-	indent(Indent),
-	write(']').
-pp(Term, Indent) :-
-	functor(Term, Name, 2),
-	current_op(_, Type, Name),
-	memberchk(Type, [xfx, yfx, xfy]), !,
-	arg(1, Term, A1),
-	arg(2, Term, A2),
-	pp(A1, Indent), format(' ~q ', [Name]), pp(A2, Indent).
-pp(Term, Indent) :-
-	functor(Term, Name, _Arity),
-	atom_length(Name, L),
-	NewIndent is Indent + L + 1,
-	format('~q(', Name),
-	(   term_argument_length(Term, AL),
-	    NewIndent + AL < 72
-	->  Wrap = nowrap
-	;   Wrap = wrap
-	),
-	forall(generate_arg(I, Term, Arg),
-	       pparg(I, Arg, Wrap, NewIndent)),
-	write(')').
-
-generate_arg(ArgN, Term, Arg) :-
-	functor(Term, _, Arity),
-	between(1, Arity, ArgN),
-	arg(ArgN, Term, Arg).
-
-pparg(1, Term, _, Indent) :- !,
-	pp(Term, Indent).
-pparg(_, Term, wrap, Indent) :- !,
-	write(','), nl,
-	indent(Indent),
-	pp(Term, Indent).
-pparg(_, Term, _, Indent) :-
-	write(', '),
-	pp(Term, Indent).
-
-longest_attribute([], L, L).
-longest_attribute([A := _|T], L0, L) :-
-	term_length(A, AL),
-	max(L0, AL, L1),
-	longest_attribute(T, L1, L).
-
-term_length(A, AL) :-
-	atom(A), !,
-	atom_length(A, AL).
-term_length(A, AL) :-
-	atomic(A), !,
-	setup_call_cleanup(open_null_stream(Out),
-			   (   write(Out, A),
-			       character_count(Out, AL)
-			   ),
-			   close(Out)).
-term_length(Var, AL) :-
-	var(Var), !,			% _
-	AL = 1.
-term_length('$VAR'(N), AL) :-
-	integer(N), !,
-	varname(N, L),
-	length(L, AL).
-term_length('$VAR'(N), AL) :-
-	atom(N), !,
-	atom_length(N, AL).
-term_length('$aref'(N), AL) :-
-	atom_length(N, AL).
-
-term_argument_length(Term, L) :-
-	term_argument_length(Term, 1, 0, L).
-
-term_argument_length(Term, A, L0, L) :-
-	arg(A, Term, Arg), !,
-	term_length(Arg, AL),
-	L1 is AL + L0,
-	NA is A + 1,
-	term_argument_length(Term, NA, L1, L).
-term_argument_length(_, _, L, L).
-
-
-max(A, B, M) :-
-	(   A >= B
-	->  M = A
-	;   M = B
-	).
-
-
-varname(N, [C]) :-
-	N < 26, !,
-	C is N + 0'A.
-varname(N, [C1, C2]) :-
-	C1 is N // 26 + 0'A,
-	C2 is mod(N, 26) + 0'A.
-
-indent(I) :-
-	Tabs is I // 8,
-	forall(between(1, Tabs, _), put(9)),
-	Spaces is mod(I, 8),
-	tab(Spaces).
+dump_var2(Name, Value) :-
+	format('~N'),
+	ansi_format([bold,fg(magenta)], '~w', [Name]),
+	format(' = '),
+	atom_length(Name, NameLen),
+	LeftMargin = NameLen + 3,
+	print_term(Value,
+		   [ output(current_output),
+		     left_margin(LeftMargin)
+		   ]),
+	nl.
