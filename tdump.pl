7cd410fc1e14d97c23c73e034fd37f98f07fdf93
Jan Wielemaker <J.Wielemaker@vu.nl>
2019-04-30 22:17:08 +0200

    Improve tdump/1

--- tdump.pl
+++ tdump.pl
@@ -18,20 +18,33 @@ tdump(M:Goal) :-
         Answer = Wrapper,
         findall(Answer-Delay, '$tbl_answer'(Trie, Skeleton, Delay), Pairs),
         sort(1, @<, Pairs, Sorted),
-        maplist(dump_answer, Sorted),
+        maplist(dump_answer(M), Sorted),
         fail
     ;   true
     ).
 
-dump_answer(Answer-[]) :-
+dump_answer(M, Answer0-true) :-
     !,
+    unqualify(Answer0, M, Answer),
     format('  ~p~n', [Answer]).
-dump_answer(Answer-Delay) :-
-    delay_condition(Delay, Condition),
-    format('  ~p IF ~p~n', [Answer, Condition]).
+dump_answer(M, Answer0-Condition) :-
+    unqualify(Answer0, M, Answer),
+    unqualify(Condition, M, SimpleCondition),
+    format('  ~p', [Answer]),
+    ansi_format(bold, ' IF ', []),
+    ansi_format(fg(cyan), '~p~n', [SimpleCondition]).
 
-delay_condition([], true).
-delay_condition([One], One) :-
+unqualify((A0,B0), M, (A,B)) :-
+    !,
+    unqualify(A0, M, A),
+    unqualify(B0, M, B).
+unqualify((A0;B0), M, (A;B)) :-
+    !,
+    unqualify(A0, M, A),
+    unqualify(B0, M, B).
+unqualify(tnot(A0), M, tnot(A)) :-
+    !,
+    unqualify(A0, M, A).
+unqualify(M:G, M, G) :-
     !.
-delay_condition([H|T0], and(H,T)) :-
-    delay_condition(T0, T).
+unqualify(G, _, G).
