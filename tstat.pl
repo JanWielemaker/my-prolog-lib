21eff76d46e1ef6dad060c095021f3408f6dad84
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2019-08-24 17:33:14 +0200

    Work on table statistics.

--- tstat.pl
+++ tstat.pl
@@ -1,19 +1,38 @@
 :- module(tstat,
-          [ table_count/1,                      % -Count
+          [ table_statistics/0,
+            table_statistics/2,                 % ?Stat, -Value
             tstat/2                             % ?Stat, ?Top
           ]).
 :- use_module(library(error)).
 :- use_module(library(aggregate)).
 :- use_module(library(solution_sequences)).
 
-table_count(NTables) :-
+
+table_statistics(Stat, Value) :-
+    (   var(Stat)
+    ->  table_statistics_(Stat, Value)
+    ;   table_statistics_(Stat, Value)
+    ->  true
+    ).
+
+table_statistics_(tables, NTables) :-
     aggregate_all(count, current_table(_:_, _), NTables).
+table_statistics_(Stat, Total) :-
+    variant_trie_stat(Stat, _What),
+    Stat \== variables,
+    aggregate_all(sum(Count), variant_stat(Stat, _Variant, Count), Total).
+
+table_statistics :-
+    forall(table_statistics(Stat, Value),
+           ( variant_trie_stat0(Stat, What),
+             format('~w ~`.t ~D~50|~n', [What, Value]))).
+
+variant_trie_stat0(tables, "Total #tables").
+variant_trie_stat0(Stat, What) :-
+    variant_trie_stat(Stat, What).
 
 tstat(Stat, Top) :-
-    (   variant_trie_stat(Stat, What, _, _)
-    *-> true
-    ;   domain_error(tstat_key, Stat)
-    ),
+    variant_trie_stat(Stat, What),
     top(Top, Count, Limit, Dir, Order),
     findall(V-Count,
             limit(Limit, order_by([Order], variant_stat(Stat, V, Count))),
@@ -35,6 +54,8 @@ variant_stat(Stat, V, Count) :-
 
 atrie_prop(T, size(Bytes)) :-
     '$trie_property'(T, size(Bytes)).
+atrie_prop(T, compiled_size(Bytes)) :-
+    '$trie_property'(T, compiled_size(Bytes)).
 atrie_prop(T, value_count(Count)) :-
     '$trie_property'(T, value_count(Count)).
 atrie_prop(T, lookup_count(Count)) :-
@@ -48,28 +69,19 @@ atrie_prop(T, gen_exit_count(Count)) :-
 atrie_prop(T, variables(Count)) :-
     '$tbl_table_status'(T, _Status, _Wrapper, Skeleton),
     functor(Skeleton, ret, Count).
-atrie_prop(T, variant_size(Size)) :-
-    '$tbl_table_status'(T, _Status, Wrapper, _Skeleton),
-    term_tsize(Wrapper, Size).
 
-term_tsize(T, Size) :-
-    compound(T),
-    !,
-    T =.. [_|Args],
-    maplist(term_tsize, Args, Sizes),
-    sum_list(Sizes, ASize),
-    Size is 1+ASize.
-term_tsize(_, 1).
+variant_trie_stat(Stat, What) :-
+    (   variant_trie_stat(Stat, What, _, _)
+    *-> true
+    ;   domain_error(tstat_key, Stat)
+    ).
 
-
-variant_trie_stat(answers,   "answer",           Count, value_count(Count)).
-variant_trie_stat(space,     "memory usage",     Bytes, size(Bytes)).
-variant_trie_stat(lookup,    "trie_lookup call", Count, lookup_count(Count)).
-variant_trie_stat(gen(call), "trie_gen call",    Count, gen_call_count(Count)).
-variant_trie_stat(gen(fail), "trie_gen fail",    Count, gen_fail_count(Count)).
-variant_trie_stat(gen(exit), "trie_gen exit",    Count, gen_exit_count(Count)).
-variant_trie_stat(variables, "variant vars",     Count, variables(Count)).
-variant_trie_stat(vsize,     "variant size",     Count, variant_size(Count)).
+variant_trie_stat(answers,        "answer",                Count, value_count(Count)).
+variant_trie_stat(space,          "memory usage",          Bytes, size(Bytes)).
+variant_trie_stat(compiled_space, "compiled memory usage", Bytes, compiled_size(Bytes)).
+variant_trie_stat(lookup,         "trie_lookup call",      Count, lookup_count(Count)).
+variant_trie_stat(gen(call),      "trie_gen call",         Count, gen_call_count(Count)).
+variant_trie_stat(variables,      "variant vars",          Count, variables(Count)).
 
 %!  write_variant_table(+Title, +Pairs)
 
