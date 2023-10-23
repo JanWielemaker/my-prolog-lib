3adaf09effa18d593d952f633117fa73b45dd402
Jan Wielemaker <J.Wielemaker@vu.nl>
2023-10-23 10:04:05 +0200

    Improve callgrind support.

--- valgrind.pl
+++ valgrind.pl
@@ -1,29 +1,40 @@
 :- module(valgrind,
-          [ vg/1,                       % :Goal
-            vg_instr/1                  % +OnOFF
+          [ cg/1,                       % :Goal
+            cg_instr/1                  % +OnOFF
           ]).
 
 :- meta_predicate
-    vg(0).
+    cg(0).
 
-%!  vg(:Goal) is semidet.
+/** <module> Callgrind support
+
+Aims at profiling using valgrind's callgrind tool. Start Prolog as below
+without instrumentation and than run cg(Goal)   to profile the execution
+of Goal.
+
+```
+valgrind --tool=callgrind --instr-atstart=no swipl ...
+```
+*/
+
+%!  cg(:Goal) is semidet.
 %
 %   Enable valgrind profiling when running Goal.   Assumes valgrind is
 %   started using ``--instr-atstart=no''.
 
-vg(Goal) :-
+cg(Goal) :-
     setup_call_cleanup(
-        vg_instr(on),
-        Goal,
-        vg_instr(off)),
+        cg_instr(on),
+        '$notrace'(Goal),               % call through PL_next_solution()
+        cg_instr(off)),
     !.
 
-%!  vg_instr(+OnOFF) is det.
+%!  cg_instr(+OnOFF) is det.
 %
 %   Enable/disable        profiling        instrumentation         using
 %   ``callgrind_control``.
 
-vg_instr(On) :-
+cg_instr(On) :-
     current_prolog_flag(pid, PID),
     process_create(path(callgrind_control),
                    [ '-i', On, PID],
