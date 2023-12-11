3864d4252aa89f82b00b8f255ec6b26d16ec8b2c
Jan Wielemaker <J.Wielemaker@vu.nl>
2023-12-11 11:12:18 +0100

    Avoid duplicate swish IDE binding

--- swish.pl
+++
@@ -1,6 +0,0 @@
-:- module(swish_ide_loader,
-	  [ swish/0,
-	    swish/1
-	  ]).
-:- expand_file_name('$HOME/src/prolog/swish/ide.pl', File),
-   use_module(File).
--- swish_ide.pl
+++ swish_ide.pl
@@ -1,7 +1,6 @@
 :- module(swish_ide_loader,
-          [ swish/0,
-            swish/1
-          ]).
-
-:- use_module('/home/janw/src/prolog/swish/ide').
-
+	  [ swish/0,
+	    swish/1
+	  ]).
+:- expand_file_name('$HOME/src/prolog/swish/ide.pl', File),
+   use_module(File).
